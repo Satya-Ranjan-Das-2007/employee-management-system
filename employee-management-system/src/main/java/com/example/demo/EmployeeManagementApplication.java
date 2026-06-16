8ceb90c5008fac784b45ab3f453b50b69992d564
25bcsi56_satya ranjan das <satyaranjanchinu@gmail.com>
2026-06-16 23:07:32 +0530

    Update EmployeeManagementApplication.java

--- employee-management-system/src/main/java/com/example/demo/EmployeeManagementApplication.java
+++ employee-management-system/src/main/java/com/example/demo/EmployeeManagementApplication.java
@@ -8,10 +8,7 @@ public class EmployeeManagementApplication {
 
     public static void main(String[] args) {
 
-        // TODO:
-        // Start Spring Boot application using SpringApplication.run()
-
-        // Hint:
-        // Pass this class name + args
-    }
+    	 SpringApplication.run(EmployeeManagementApplication.class, args);
+    	 System.out.println("Springboot Successfully launched!");    
+    	 }
 }
